1edc354fe142afb4be41d58f1ed58d7e7946fb3d
cammcgary <cammcgary@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-04-12 03:28:55 +0000

    477 - Added ODF and TEX, don't know which one we'll use, DC Voltage/Current meter preliminary written, **SHOULD** work

--- trunk/ece477/final/atod.c
+++ trunk/ece477/final/atod.c
@@ -7,18 +7,32 @@
 
 /*This file contains methods for initializing the A-D converter
  * and making A-D readings, it will return the A-D result as an unsigned int
+ NOTE, this code NO LONGER TRUNCATES THE A/D, IT IS RETURNED AS 16bit INT
+ The actual A/D is only 10 bits, so the top 6 are 0
  */
 
-char adconvert(void){
-	//ADLAR = ADLAR | 0b00100000; //Set ADLAR Left adjust bit
-	//ADMUX &= 0b00010000;	//Leave bit 4 alone
-    //ADMUX |= 0b00000101;    //Select AD5 (Pin 28 for A/D conversion)
-    ADMUX = 0b11100101; //This performs the job of the above 3 lines
-
+uint16_t adconvert(int adcx){
+	uint16_t adclh = 0;	//ADC return value
+	int hold;
+	//Set internal 1.1v VRef with external capacitor req'd at AREF
+    ADMUX = 0b1100000;
+	if(adcx < 8) ADMUX |=adcx; //If adcx >7, other bits will be screwed up.
+	else return(-1);			//Return -1 if failed due to poor adcx select
+	
+	//The following actually does the conversion
 	PRR &=~(1<<PRADC);
 	ADCSRA |=(1<<ADSC);	//ADC Start conversion bit
 	while(ADCSRA&_BV(ADSC)); //Wait for A/D Conversion
 	//BV does bit value
-	return(ADCH);	//return most sig. 8 bits.
+	
+	//ADCL should be read first, reading ADCH re-enables writing to
+	//the ADC output registers, so it is done last for safety
+	
+	hold = ADCL;
+	adclh |= ADCH;
+	adc1h = adc1h<<8;
+	adclh |= hold
+	
+	return(adc1h);	//return 10 bit ADC in 16 bit signed INT
 }
 
