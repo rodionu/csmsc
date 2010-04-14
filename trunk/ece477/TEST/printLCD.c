045c74973214d4dc4785a1c97a39bab399fa0cfa
castro2kaiba <castro2kaiba@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-04-14 05:26:42 +0000

    binary to hex and strings

--- trunk/ece477/TEST/printLCD.c
+++ trunk/ece477/TEST/printLCD.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <avr/io.h>
 #include <string.h>
+#include <strings.h>
 #include "lcd_setup.h"
 #include "m88delay.h"
 #include "printLCD.h"
@@ -9,14 +10,16 @@
 int printLCD(char *buf){
 	int cur;
 	for(cur = 0; cur < strlen(buf); cur++){
+		PORTB &= 0x3F;	// 0b00111111;
+		PORTD = 0x01;	// 0b00000001
 		m88delay(1);
-		PORTB &= 0b10111111;
-		PORTB |= 0b10000000;
+		PORTB &= 0xBF;	// 0b10111111;
+		PORTB |= 0x80	// 0b10000000;
 		PORTD = buf[cur];
 		EHIGH();
 		ELOW();
-		PORTB &= 0b00111111;
-		PORTD = 0b00010100;
+		PORTB &= 0x3F;	// 0b00111111;
+		PORTD = 0x14;	// 0b00010100;
 		EHIGH();
 		ELOW();
 	}
