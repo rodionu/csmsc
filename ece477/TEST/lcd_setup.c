0499b6064b7a35565e158d4b89b295cbfe9e62b7
castro2kaiba <castro2kaiba@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-04-14 17:03:20 +0000

    delay changed

--- ece477/TEST/lcd_setup.c
+++ ece477/TEST/lcd_setup.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <avr/io.h>
+#include <util/delay.h>
 #include "lcd_setup.h"
-#include "m88delay.h"
 
 #define E 5;
 #define RW 6;
@@ -13,39 +13,39 @@ void lcd_init(void){
 	//DDRB |= 0xE0;	// enable last 3 bits of PORT B for enable, read/write, etc
 	
 	/* LCD Initialization for 16166 LCD used */
-	m88delay(20);
+	_delay_ms(20);
 	ELOW();			// Set enable low for starting
 	PORTB &= 0x3F;	// 0b00111111
 	PORTD = 0x38;	// 0b00111000	// 1, 1 for 8 bit, 1 for 16:1 mux
 	EHIGH();
 	ELOW();
-	m88delay(5);
+	_delay_ms(5);
 	PORTB &= 0x3F;  // 0b00111111
     PORTD = 0x38;   // 00111000 // 1, 1 for 8 bit, 1 for 16:1 mux
 	EHIGH();
 	ELOW();
-	m88delay(1);
+	_delay_ms(1);
 	EHIGH();
 	ELOW();
-//	m88delay(1);
+//	_delay_ms(1);
 //  EHIGH();
 //  ELOW();
-	m88delay(1);
+	_delay_ms(1);
 	PORTB &= 0x3F;	// 0b00111111
 	PORTD = 0x01;	// 0b00000001	Clear DD RAM and LCD
 	EHIGH();
 	ELOW();
-	m88delay(20);
+	_delay_ms(20);
 	PORTB &= 0x3F;	// 0b00111111
 	PORTD = 0x06;	// 0b00000110	// 1, 1 for increment, 0 for no shift
 	EHIGH();
 	ELOW();
-	m88delay(1);
+	_delay_ms(1);
 	PORTB &= 0x3F;	// 0b00111111
 	PORTD = 0x0E;	// 0b00001100	// 1,1 for Display on, 1 for cursor off
 	EHIGH();
 	ELOW();
-	m88delay(10);
+	_delay_ms(10);
 }
 
 void ELOW(void){
--- ece477/TEST/printLCD.c
+++ ece477/TEST/printLCD.c
@@ -2,16 +2,15 @@
 #include <avr/io.h>
 #include <string.h>
 #include "lcd_setup.h"
-#include "m88delay.h"
 #include "printLCD.h"
-
+#include <util/delay.h>
 
 int printLCD(char *buf){
 	int cur;
 	for(cur = 0; cur < strlen(buf); cur++){
 		PORTB &= 0x3F;	// 0b00111111;
 		PORTD = 0x01;	// 0b00000001
-		m88delay(1);
+		_delay_ms(1);
 		PORTB &= 0xBF;	// 0b10111111;
 		PORTB |= 0x80;	// 0b10000000;
 		PORTD = buf[cur];
