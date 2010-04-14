0033b03df77ba7d0f115e3261cb1875c22dd2cbb
cammcgary <cammcgary@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-04-14 06:12:38 +0000

    serial works on printlcd.c

--- ece477/TEST/printLCD.c
+++ ece477/TEST/printLCD.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <avr/io.h>
 #include <string.h>
-#include <strings.h>
 #include "lcd_setup.h"
 #include "m88delay.h"
 #include "printLCD.h"
@@ -14,7 +13,7 @@ int printLCD(char *buf){
 		PORTD = 0x01;	// 0b00000001
 		m88delay(1);
 		PORTB &= 0xBF;	// 0b10111111;
-		PORTB |= 0x80	// 0b10000000;
+		PORTB |= 0x80;	// 0b10000000;
 		PORTD = buf[cur];
 		EHIGH();
 		ELOW();
