ad48c3d0a228c79375c1b7557ba619005ddd0555
castro2kaiba <castro2kaiba@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-04-15 00:43:48 +0000

    Repalced EHIGH ELOW w/ EHIT

--- trunk/ece477/TEST/printLCD.c
+++ trunk/ece477/TEST/printLCD.c
@@ -8,20 +8,17 @@
 int printLCD(char *buf){
 	int cur;
 	for(cur = 0; cur < strlen(buf); cur++){
-		PORTB &= 0x3F;	// 0b00111111;
+		PORTB &= 0x1F;	// 0b00111111;
 		PORTD = 0x01;	// 0b00000001
-		EHIGH();
-		ELOW();
+		EHIT();
 		_delay_ms(50);
 		PORTB &= 0xBF;	// 0b10111111;
 		PORTB |= 0x80;	// 0b10000000;
 		PORTD = buf[cur];
-		EHIGH();
-		ELOW();
-		PORTB &= 0x3F;	// 0b00111111;
+		EHIT();
+		PORTB &= 0x1F;	// 0b00111111;
 		PORTD = 0x14;	// 0b00010100;
-		EHIGH();
-		ELOW();
+		EHIT();
 	}
 	return 0;	
 }
