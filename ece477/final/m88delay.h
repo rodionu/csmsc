fd34f2224f3d07a2a81ccdbb24619e13eccd2d72
cammcgary <cammcgary@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-04-15 06:01:04 +0000

    removed m88delay and fixed makefile

--- ece477/final/m88delay.h
+++
@@ -1,5 +0,0 @@
-//The 8MHz/8000 = 1000 cycles/second, compare statements likely take
-//more than just one cycle to execute, so this is actually around
-//a 1-2ms delay as it is now.
-
-void m88delay(unsigned int ms);
