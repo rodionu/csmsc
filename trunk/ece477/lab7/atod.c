03823ba6444efd0b2cec64fbdbf5d988042808d5
castro2kaiba <castro2kaiba@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-04-15 18:28:05 +0000

    finished LCD part of write up and presentation

--- trunk/ece477/lab7/atod.c
+++ trunk/ece477/lab7/atod.c
@@ -13,7 +13,7 @@ char adconvert(void){
 	//ADLAR = ADLAR | 0b00100000; //Set ADLAR Left adjust bit
 	//ADMUX &= 0b00010000;	//Leave bit 4 alone
     //ADMUX |= 0b00000101;    //Select AD5 (Pin 28 for A/D conversion)
-    ADMUX = 0b11100101; //This performs the job of the above 3 lines
+    ADMUX = 0b11100011; //This performs the job of the above 3 lines
 
 	PRR &=~(1<<PRADC);
 	ADCSRA |=(1<<ADSC);	//ADC Start conversion bit
