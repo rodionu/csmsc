e63d1fe1a8264fdf3024f00c88322ceec0f3fabd
cammcgary <cammcgary@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-05-04 19:37:34 +0000

    fixed the plant model, should be set now

--- trunk/ece414/project.m
+++ trunk/ece414/project.m
@@ -4,11 +4,15 @@ s = tf('s');
 %Plant model for MOTOR 1
 %G = tf((-3.348e4*s^2+3.164e-9*s-2.938e5)/(s^5+320.1*s^4+3.579e4*s^3));
 %Plant model for MOTOR 3 (used)
-G = tf((3.1e4*s^2-1.012e-9*s-2.72e5)/(s^5+533.4*s^4+3.684e4*s^3));
-F = tf((s+10)/((s+20+5j)*(s+20-5j)));          %Prefilter
-H = tf((s+1)/(s*(s+1)));           %Feedback amp
-L = G*F;        %Forward path gain
-T = L/(1+G*H);  %CLTF
+gze = [0+2.9624*j 0-2.9624*j];
+gdp = [0 0 0 -451.8660 -81.5204];
 
-[num,den] = tfdata(L,'v');
-rlocus(num,den,0:.1:1000);
+G = zpk(gze,gdp,1);
+
+F = tf((s+50)/(s+200));          %Prefilter
+H = tf((s+1)/(s*(s+1)));           %Feedback amp
+%L = G*F;        %Forward path gain
+%T = L/(1+G*H);  %CLTF
+
+%[num,den] = tfdata(L,'v');
+%rlocus(num,den,0:.1:1000);
