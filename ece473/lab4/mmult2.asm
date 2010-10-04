4ce9d44e324387f3e14f3fcff90c79348a566eb0
cammcgary <cammcgary@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-10-04 21:47:16 +0000

    Added new drafts, mmult3 is the one to use

--- ece473/lab4/mmult2.asm
+++ ece473/lab4/mmult2.asm
@@ -5,6 +5,7 @@
 .data
 aMatrix:	.word 1 2 -1 3 0 4
 bMatrix:	.word 1 -4 2 5 -3 6
+cMatrix	.word 0 0 0 0
 aXdim:		.word 3		#Row length
 aYdim:		.word 2		#Column length
 bXdim:		.word 2
@@ -14,27 +15,18 @@ cYdim:		.word 0
 
 #What all the registers in this program are used for
 #a0-a4: 	Storing aX/aY and bX/bY dimensions
-#t0:		Counting the current element in the matrix multiply, BEFORE SUM
+#t0:		Column select for A, Row select for B
 #t1-t2:	Storing the elements to be multiplied in A and B
-
-
+#t4:		Temporary multiplication storage
+#k0-k1:	Tracking current row in A and column in B (nested loops)
+#s0:		Used for saving stack pointer
+#t9:		Temporary offset calculation storage
+#v0:		Row offset to aMatrix (row-select)
+#v1:		Column offset to bMatrix (column-select)
+#t8:		A address register
+#t9:		B address register
 
 main:
-
-	lw $a0,aXdim		#Determine row length of resultant matrix
-	lw $a1,bXdim
-	blt $a0,$a1,cXaX	#Conditional statement setting cXdim
-	sw $a1,cXdim		#Else statement
-	j skipcX		
-cXaX:	sw $a0,cXdim		#Conditional true
-
-skipcX:	lw $a0,aYdim		#Repeating the above statements
-	lw $a1,bYdim			#Determine column length of resultant matrix
-	blt $a0,$a1,cYaY	
-	sw $a1,cYdim		
-	j skipcY		
-cYaY:	sw $a0,cYdim
-
 #This section establishes the width and length of the matrices
 #in byte amounts, so they can be used as stop-limiters
 #All A REGISTERS ARE USED FOR THIS, DO NOT TOUCH
@@ -49,35 +41,35 @@ cYaY:	sw $a0,cYdim
 	li $k0,0	#Used for tracking current row in aMatrix
 	li $k1,0	#Used for tracking current column in bMatrix
 
-$#This section calculates a single element of the resultant matrix
+	lw $s1,aYdim		#Load #rows*4 into s1, so we know how many times to rpt
+	li $v0,0			#A-Row select
+	li $v1,0			#B-Column select
+
+#This section calculates a single element of the resultant matrix
 #one ROW in aMatrix and one COLUMN in bMatrix
 
-		lw $s1,aYdim		#Load #columns*4 into sv1, so we know how many times to rpt
-		li $v0,0		#Row offset to aMatrix
-		li $v1,0		#Column offset to bMatrix
-
-ele:		li $t0,0		#Initialization
+ele:		li $t0,0			#Initialization (count = 0)
 		la $s0,$sp		#Save stack pointer (will need it later)
 
 mults:	
 		add $v0,$v0,$t0		#Select element in aMatrix
-		mult $t0,$a2		#Offset of ONE ROW * ele number in bMatrix
-		mflo $t9
-		add $v1,$v1,$t9		#Determines full offset in bMatrix
+		mult $t0,$a2			#Offset of ONE ROW * ele number in bMatrix
+		mflo $t4
+		add $v1,$v1,$t4		#Determines full offset in bMatrix
 
 		lw $t1,aMatrix($v0)	#Should be Matrix offset by t0 (0 to start)
 		lw $t2,bMatrix($v1)
 		mult $t1,$t2		#Multiply current element pair.
-		mflo $t3		#Store 32 bit result
+		mflo $t3			#Store 32 bit result
 		sw $t3,$sp		#Hold result on the stack for now
 		subi $sp,$sp,4		#Decrement stack pointer
 		addi $t0,$t0,4		#Add 4 to $a0
 		blt $t0,$a0,mults	#Repeat until you calculate the last element
 
-		lw $t1,$sp	#Load top of stack
-stack:	addi $sp,$sp,4	#Increment stack ptr
-		lw $t2,$sp	#Load register with next stack element
-		add $t1,$t1,$t3	#Add (to eventually get matrix result element)
+		lw $t1,$sp			#Load top of stack
+stack:	addi $sp,$sp,4		#Increment stack ptr
+		lw $t2,$sp			#Load register with next stack element
+		add $t1,$t1,$t3		#Add (to eventually get matrix result element)
 		blt $sp,$s0,stack	#Make sure we don't go past the starting address
 
 #Now we need to save the resultant number into the c matrix, and restart
