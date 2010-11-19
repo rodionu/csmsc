86f3190592b93a06e99649abaf5d359bce740423
cammcgary <cammcgary@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2010-11-19 02:16:31 +0000

    Forgot to make changes in Proj1 folder, Test1 folder added to 473 (this was as far as we got it to work)

---
+++ trunk/ece473/Test1/LEDend.v
@@ -0,0 +1,18 @@
+	module LEDend(
+			Val,
+			done	
+		);
+		input Val;
+		output done;
+		
+		reg [17:0] done;
+		
+		
+		always @* begin
+			if(Val) begin
+				done=18'b111111111111111111;
+			end else begin
+				done=18'b000000000000000000;
+			end
+		end
+	endmodule
