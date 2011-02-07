01100566e4416993ba94dce2835060f86610d9f0
cammcgary <cammcgary@f94e2a46-17fe-11df-b2b6-d9041abd8731>
2011-02-07 05:12:49 +0000

    fixed

--- ece473/proj2/Register.v
+++ ece473/proj2/Register.v
@@ -7,12 +7,14 @@ input wire [31:0] Instruction,
 input wire RegWrite,	//Write enable		//From Pipe STAGE 4
 input wire [4:0] WN,		//Write Register #	//From Pipe STAGE 4
 input wire [31:0] WD,	//Write data		//From Pipe STAGE 4
+output reg [4:0] RS,
+output reg [4:0] RT,
 output reg [31:0] RD1,	//Output reg 1
 output reg [31:0] RD2);	//Output reg 2
 
 //Below are variables NOT input/output ports
-reg [4:0] RS;		//First input
-reg [4:0] RT;		//Second input
+//reg [4:0] RS;		//First input
+//reg [4:0] RT;		//Second input
 reg [31:0] mem [0:31];	//32x32bit memory "register"
 reg [4:0] i;		//Loop variable
 
@@ -26,6 +28,17 @@ always @* begin			//Trigger on the clock
 		end
 		
 	end else if(RESET == 0) begin
+<<<<<<< .mine
+		if(RegWrite == 1) begin		//Writebacks can be read in same cycle
+			mem[WN] = WD;			//Write data to register
+		end
+		RS = Instruction[25:21];		//First input
+		RT = Instruction[20:16];		//Second input
+		RD1 = mem[RS];		//Verilog array elements are
+		RD2 = mem[RT];		//accessed "all bits" at a time, these
+	end
+						//will be 32 bit numbers
+=======
 		if(RegWrite!=0) begin		//Writebacks can be read in same cycle
 			mem[WN] = WD;			//Write data to register
 			RS = Instruction[25:21];		//First input
@@ -38,7 +51,7 @@ always @* begin			//Trigger on the clock
 			RD1 = mem[RS];		//Verilog array elements are
 			RD2 = mem[RT];		//accessed "all bits" at a time, these
 								//will be 32 bit numbers
-	end
+>>>>>>> .r131
 end
 end
 endmodule
